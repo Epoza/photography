a2c3cceaa8ca43ad7a9ad151b1074d2150523a74
Epoza <86025026+Epoza@users.noreply.github.com>
2025-03-05 21:32:17 -0500

    Add basic HTML

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <>
-      <div>Home section</div>
+      <div className="bg-orange-950 h-[calc(100vh-4.5rem)]">
+        <div>Home Section</div>
+        <Link
+          className="p-2 border-1 border-b-amber-100"
+          key={"gallery-link"}
+          to={"/gallery"}
+        >
+          Go To Gallery
+        </Link>
+      </div>
     </>
   );
 };
