a347e3e5794ffb62b7465734860b23a5ae6be74c
Epoza <86025026+Epoza@users.noreply.github.com>
2025-03-05 21:29:39 -0500

    Add grid for images

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,7 +1,22 @@
+const imagePaths = import.meta.glob("/src/assets/images/*.{jpg,png,webp}");
+const images = Object.keys(imagePaths);
+
 const Gallery = () => {
   return (
     <>
       <div>Gallery section</div>
+      <div className="columns-2 gap-4 p-4">
+        {images.map((image, index) => (
+          <div key={index} className="overflow-hidden shadow-lg mb-4">
+            <img
+              src={image}
+              alt={`Gallery ${index}`}
+              className="w-full object-cover transition-transform duration-300 hover:scale-105"
+              loading="lazy"
+            />
+          </div>
+        ))}
+      </div>
     </>
   );
 };
