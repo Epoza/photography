de7a3f371520caef2015746c1484745201931ef0
Epoza <86025026+Epoza@users.noreply.github.com>
2025-03-11 22:08:23 -0400

    Add navigation to the different galleries

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,24 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Gallery from "./components/Gallery";
 import About from "./components/About";
+import Animals from "./components/gallerySections/Animals";
+import Landscapes from "./components/gallerySections/Landscapes";
 
 const App = () => {
   return (
     <>
       <Router>
         {/* Put components here such as Navbar, footer, homepage, etc.*/}
+        {/* Possible move header into each component home, gallery, About */}
         <Header />
-        <div className="text-3xl underline">App</div>
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/gallery" element={<Gallery />}>
+            {/* Nested routes */}
+            <Route path="animals" element={<Animals />} />
+            <Route path="landscapes" element={<Landscapes />} />
+          </Route>
           <Route path="/about" element={<About />} />
         </Routes>
       </Router>
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,11 +1,30 @@
-import Animals from "./gallerySections/Animals";
-import Landscapes from "./gallerySections/Landscapes";
+import { Link, useLocation, Outlet } from "react-router-dom";
 
 const Gallery = () => {
+  const location = useLocation();
+
   return (
-    <div>
-      <Animals />
-      <Landscapes />
+    <div className="p-4">
+      {/* Show buttons ONLY if on /gallery */}
+      {location.pathname === "/gallery" && (
+        <div className="flex gap-4 mb-4">
+          <Link
+            to="/gallery/animals"
+            className="px-4 py-2 rounded-lg bg-gray-300 hover:bg-blue-500 hover:text-white"
+          >
+            Animal Images
+          </Link>
+          <Link
+            to="/gallery/landscapes"
+            className="px-4 py-2 rounded-lg bg-gray-300 hover:bg-blue-500 hover:text-white"
+          >
+            Landscape Images
+          </Link>
+        </div>
+      )}
+
+      {/* This will render either Animals or Landscapes when navigated to */}
+      <Outlet />
     </div>
   );
 };
