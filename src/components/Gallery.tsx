3ce7795469ad57510e669fd148db5b80550d4d3e
Epoza <86025026+Epoza@users.noreply.github.com>
2025-03-13 22:43:53 -0400

    work on adding clickable cards

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -4,21 +4,41 @@ const Gallery = () => {
   const location = useLocation();
 
   return (
-    <div className="p-4">
+    <div className="w-full h-[calc(100vh-4.5rem)] bg-soft-cream">
       {/* Show buttons ONLY if on /gallery */}
       {location.pathname === "/gallery" && (
-        <div className="flex gap-4 mb-4">
+        // grid container
+        <div
+          id="container"
+          className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4 p-4"
+        >
           <Link
             to="/gallery/animals"
-            className="px-4 py-2 rounded-lg bg-gray-300 hover:bg-blue-500 hover:text-white"
+            className="w-[300px] h-[400px] flex flex-col items-center border-2 border-dark-coffee rounded-lg overflow-hidden transition hover:border-light-oat"
           >
-            Animal Images
+            <img
+              src="/src/assets/images/animals/preview.jpg"
+              alt="Animal Gallery"
+              className="w-full h-48 object-cover"
+            />
+            <p className="w-full p-4 text-center bg-light-oat text-dark-coffee">
+              Animal Images
+            </p>
           </Link>
+
           <Link
             to="/gallery/landscapes"
-            className="px-4 py-2 rounded-lg bg-gray-300 hover:bg-blue-500 hover:text-white"
+            className="w-[300px] h-[300px] flex flex-col items-center border-2 border-dark-coffee rounded-lg overflow-hidden transition hover:border-medium-latte hover:opacity-80"
           >
-            Landscape Images
+            <img
+              src="/src/assets/images/landscapes/DSC_3591.JPG"
+              // change alt tag
+              alt="Landscape Gallery"
+              className="w-full h-[250px] object-cover"
+            />
+            <p className="w-full p-4 text-center bg-light-oat text-dark-coffee">
+              Landscape Images
+            </p>
           </Link>
         </div>
       )}
