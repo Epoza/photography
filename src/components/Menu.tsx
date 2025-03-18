949c46621999deaa7be04246689a04923852331e
Epoza <86025026+Epoza@users.noreply.github.com>
2025-03-17 23:05:27 -0400

    add social media links

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,13 +4,13 @@ import { PiInstagramLogo } from "react-icons/pi";
 
 const Menu = ({ closeMenu }: { closeMenu: () => void }) => {
   return (
-    <div className="absolute top-full left-0 w-full h-[calc(100vh-4.5rem)] bg-amber-700 text-white flex flex-col items-center justify-center p-4 shadow-lg">
+    <div className="absolute top-full left-0 w-full h-[calc(100vh-4.5rem)] bg-light-oat flex flex-col items-center justify-center p-4 shadow-lg">
       <Navigation className="py-4 text-xl" onClick={closeMenu} />
       <div className="flex flex-row gap-6 py-4">
-        <a href="https://instagram.com">
+        <a href="https://instagram.com/Photo.Repeat/">
           <PiInstagramLogo size={22} />
         </a>
-        <a href="mailto:">
+        <a href="mailto:malarie1012@gmail.com">
           <MdOutlineMail size={22} />
         </a>
       </div>
