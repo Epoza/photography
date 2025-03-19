d2fcf542077bad6d45e5389e728c1c45300639cc
Epoza <86025026+Epoza@users.noreply.github.com>
2025-03-18 21:46:32 -0400

    change text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,8 @@
 const About = () => {
   return (
     <>
-      <div className="bg-blue-900 h-[calc(100vh-4.5rem)]">
-        <div>About Section</div>
+      <div className="bg-light-oat h-[calc(100vh-4.5rem)]">
+        <div>About Section. Coming soon... maybe...</div>
       </div>
     </>
   );
