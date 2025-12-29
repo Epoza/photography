d5a82217733de178adc6f997349a403eb2780c07
epoza <86025026+Epoza@users.noreply.github.com>
2025-12-29 15:23:47 -0500

    add new galleries

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,4 +1,13 @@
 import { Link, useLocation, Outlet } from "react-router-dom";
+import animalImage from "../images/birds/DSC_4120.JPG";
+import littleImage from "../images/little_guys/DSC_0281.JPG";
+import bigImage from "../images/big_boys/DSC_4710.JPG";
+import landscapeImage from "../images/landscapes/DJI_0238.JPG";
+import flowerImage from "../images/flowers/DSC_3120.JPG";
+import boatImage from "../images/boats/DSC_2995.JPG";
+import skyImage from "../images/sky/DSC05802.JPG";
+import sunriseImage from "../images/sunrise/DSC07305.JPG";
+import Thumbnail from "./Thumbnail";
 
 const Gallery = () => {
   const location = useLocation();
@@ -12,35 +21,22 @@ const Gallery = () => {
           id="container"
           className="min-h-[calc(100vh-4.5rem)] w-full grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] auto-rows-min gap-4 p-4 justify-start"
         >
-          <Link
-            to="/gallery/animals"
-            className="max-w-[300px] max-h-[300px] flex flex-col items-center border-2 border-dark-coffee rounded-lg overflow-hidden transition hover:border-medium-latte hover:opacity-80"
-          >
-            <img
-              src={`${import.meta.env.BASE_URL}/images/animals/DSC_0303.JPG`}
-              alt="Animal Gallery"
-              className="w-full h-[240px] object-cover"
-            />
-            <p className="w-full p-4 text-center bg-light-oat text-dark-coffee">
-              Animal Images
-            </p>
-          </Link>
-
-          <Link
-            to="/gallery/landscapes"
-            className="max-w-[300px] max-h-[300px] flex flex-col items-center border-2 border-dark-coffee rounded-lg overflow-hidden transition hover:border-medium-latte hover:opacity-80"
-          >
-            <img
-              src={`${import.meta.env.BASE_URL}/images/landscapes/DSC_4580.JPG`}
-              // change alt tag
-              alt="Landscape Gallery"
-              // image has max width of 240px
-              className="w-full h-[240px] object-cover"
-            />
-            <p className="w-full p-4 text-center bg-light-oat text-dark-coffee">
-              Landscape Images
-            </p>
-          </Link>
+          {/* Birds */}
+          <Thumbnail to="/gallery/birds" imageSrc={animalImage} title="Bird Images"/>
+          {/* Little Animals */}
+          <Thumbnail to="/gallery/little_guys" imageSrc={littleImage} title="Little Animal Images"/>
+          {/* Big Animals */}
+          <Thumbnail to="/gallery/big_boys" imageSrc={bigImage} title="Big Animal Images"/>
+          {/* landscapes */}
+          <Thumbnail to="/gallery/landscapes" imageSrc={landscapeImage} title="Landscape Images"/>
+          {/* boats */}
+          <Thumbnail to="/gallery/boats" imageSrc={boatImage} title="Boat Images"/>
+          {/* flowers */}
+          <Thumbnail to="/gallery/flowers" imageSrc={flowerImage} title="Flower Images"/>
+          {/* sky */}
+          <Thumbnail to="/gallery/sky" imageSrc={skyImage} title="Sky Images"/>
+          {/* sunrise */}
+          <Thumbnail to="/gallery/sunrise" imageSrc={sunriseImage} title="Sunrise Images"/>
         </div>
       )}
 
