320d3af3dd9fda4cdd142d3a07b971551d6ed8c1
epoza <86025026+Epoza@users.noreply.github.com>
2025-12-29 15:21:50 -0500

    update route

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,21 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Gallery from "./components/Gallery";
 import About from "./components/About";
-import Animals from "./components/gallerySections/Animals";
-import Landscapes from "./components/gallerySections/Landscapes";
 import ImageView from "./components/gallerySections/ImageView";
 import Pricing from "./components/Pricing";
+import GalleryRoute from "./components/gallerySections/galleryRoute";
 
 const App = () => {
   return (
     <>
       <Router basename="/photography">
-        {/* Put components here such as Navbar, footer, homepage, etc.*/}
-        {/* Possible move header into each component home, gallery, About */}
         <Header />
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/gallery" element={<Gallery />}>
-            {/* Nested routes */}
-            <Route path="animals" element={<Animals />} />
-            <Route path="landscapes" element={<Landscapes />} />
-            <Route path="/gallery/image/:id" element={<ImageView />} />
+            <Route path=":category" element={<GalleryRoute />} />
+            <Route path="image/:id" element={<ImageView />} />
           </Route>
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/about" element={<About />} />
