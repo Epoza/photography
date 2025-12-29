92a7d2fe0f94fef9e8b0e26f9b839ada1b2688e5
epoza <86025026+Epoza@users.noreply.github.com>
2025-12-29 15:53:15 -0500

    fix GitHub Pages URL issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 
 import Home from "./components/Home";
@@ -11,7 +11,7 @@ import GalleryRoute from "./components/gallerySections/galleryRoute";
 const App = () => {
   return (
     <>
-      <Router basename="/photography">
+      <Router>
         <Header />
 
         <Routes>
